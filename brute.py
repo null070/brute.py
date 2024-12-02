81976f9ea914ff41c58ebdba0e5ff3c0ef800786
null070 <victordanielll42@gmail.com>
2024-12-02 16:08:17 -0300

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,7 +1,10 @@
 from random import *
 import os
 b = input('senha:')
-pwd = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+pwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+  '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
+  '!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '/', '?', '|', '\\'
+]
 
 pw=""
 while(pw!=b):
@@ -9,7 +12,10 @@ while(pw!=b):
     for letter in range(len(b)):
         guess_pwd = pwd[randint(0,17)]
         pw=str(guess_pwd)+str(pw)
-        print(pw)
+        print("tentando "pw)
         print('descobrindo...')
+        
         os.system("clear")
+        
 print("sua senha é: ", pw)
+
