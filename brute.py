83bae081cf2b8a170e0d20dfa2fe5881acd69e35
null070 <victordanielll42@gmail.com>
2024-12-02 16:17:17 -0300

    Update brute.py

--- brute.py
+++ brute.py
@@ -1,21 +1,23 @@
-from random import *
+from random import choice
 import os
-b = input('senha:')
-pwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+
+# Entrada da senha
+b = input('Digite sua senha: ')
+
+# Lista de caracteres possíveis
+pwd = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 
   '!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', ',', '.', '/', '?', '|', '\\'
 ]
 
-pw=""
-while(pw!=b):
-    pw=""
-    for letter in range(len(b)):
-        guess_pwd = pwd[randint(0,17)]
-        pw=str(guess_pwd)+str(pw)
-        print("tentando ", pw)
-        print('descobrindo...')
-        print("obrigado por usar minha ferramenta!")
-        os.system("clear")
-        
-print("sua senha é: ", pw)
+pw = ""
+while pw != b:
+    pw = ""
+    for _ in range(len(b)): 
+        pw += choice(pwd) 
+    print("Tentando:", pw)
+    print("Descobrindo...")
+    os.system("clear")
 
+print("Senha encontrada:", pw)
