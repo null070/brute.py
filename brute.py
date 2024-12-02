b8eb41058bd1787bab22cfd0b7b4816f02687e19
null070 <victordanielll42@gmail.com>
2024-12-02 15:45:25 -0300

    Add files via upload

---
+++ brute.py
@@ -0,0 +1,15 @@
+from random import *
+import os
+b = ('senha:')
+pwd = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+pw=""
+while(pw!=b):
+    pw=""
+    for letter in range(len(b)):
+        guess_pwd = pwd[randint(0,17)]
+        pw=str(guess_pwd)+str(pw)
+        print(pw)
+        print('descobrindo...')
+        os.system("clear")
+print("sua senha é: ", pw)
